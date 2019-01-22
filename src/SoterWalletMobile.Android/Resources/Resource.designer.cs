4722081b82e3c9391a6f885883273e8f0c634077
Zhen Liu <liuzhen@touchjet.com>
2019-01-22 11:39:51 +0800

    Add Android app icon files.

--- src/SoterWalletMobile.Android/Resources/Resource.designer.cs
+++ src/SoterWalletMobile.Android/Resources/Resource.designer.cs
@@ -3943,12 +3943,6 @@ namespace SoterWalletMobile.Droid
 			// aapt resource value: 0x7f030000
 			public const int icon = 2130903040;
 			
-			// aapt resource value: 0x7f030001
-			public const int icon_round = 2130903041;
-			
-			// aapt resource value: 0x7f030002
-			public const int launcher_foreground = 2130903042;
-			
 			static Mipmap()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
